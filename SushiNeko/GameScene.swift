f5fe2debf60cf5969155092935e726ae15c3adc0
Donghyun <markk628@gmail.com>
2020-02-06 11:31:24 -0800

    forgot to make sushibase parent of chopsticks

--- SushiNeko/GameScene.swift
+++ SushiNeko/GameScene.swift
@@ -14,5 +14,16 @@ enum Side {
 }
 
 class GameScene: SKScene {
+    /* Game objects */
+    var sushiBasePiece: SushiPiece!
     
+    override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        
+        /* Connect game objects */
+        sushiBasePiece = childNode(withName: "sushiBasePiece") as! SushiPiece
+        /* Setup chopstick connections */
+        sushiBasePiece.connectChopsticks()
+
+    }
 }
